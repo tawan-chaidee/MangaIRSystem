da0d644cdbea16808dcca41399e580637b38d39c
pastelite <kana.bhong@gmail.com>
2023-11-24 22:27:47 +0700

    new scraper

--- Tools/scraper_func.ts
+++ Tools/scraper_func.ts
@@ -1,85 +1,13 @@
+import chalk from "chalk";
 import puppeteer, { Browser } from "puppeteer";
 import fs from "fs";
-import * as csv from "fast-csv";
 import https from "https";
 import path from "path"
 import dotenv from "dotenv"
-dotenv.config({path: path.resolve("../.env")})
-import chalk from "chalk";
+dotenv.config({ path: path.resolve("../.env") })
 
-const MAX_CONCURRENT_TASKS = 1; //GOING TOO FAST
-const START_INDEX = 0;
-const DATA_SIZE = 50;
-const DELAY = 0; // Delay for each manga in milisecond
-const WANTED_GENRE = 'Fantasy';
-const JSON_FILE_PATH = 'manga_data.json';
-
-// EventEmitter.defaultMaxListeners = MAX_CONCURRENT_TASKS;
-process.setMaxListeners(0);
-const csvData = fs.createReadStream('./manga_dataset.csv');
-const results: any[] = [];
-
-csv.parseStream(csvData, { headers: true })
-  .on('data', (data: { [x: string]: any }) => {
-    // Remove non-Manga entries
-    if (data['Type'] !== 'Manga')
-      return;
-
-    // convert Members and Score to number
-    let { Members, Score, ...rest } = data;
-
-    results.push({
-      Members: parseIntImproved(Members),
-      Score: parseFloatImproved(Score),
-      ...rest
-    });
-  })
-  .on('end', async () => {
-    // Sort by Members
-    results.sort((a, b) => b.Members - a.Members).map((entry, index) => entry['index'] = index);
-    console.log(results)
-
-    const browser = await puppeteer.launch({ headless: true });
-    const scrapeTasks = results.slice(START_INDEX, START_INDEX + DATA_SIZE).map((entry) => {
-      let index = entry['index'];
-      const title = entry['Title'];
-      const url = entry['page_url'];
-      return () => scrapeMangaData(browser, url, title, index);
-    });
-
-    const mangaDataArray = await runInParallel(scrapeTasks, MAX_CONCURRENT_TASKS);
-
-    // Write scraped data to JSON file
-    writeJsonFile(JSON_FILE_PATH, mangaDataArray);
-
-    console.log('Scraped data has been written to:', JSON_FILE_PATH);
-    console.log('Total manga scraped:', mangaDataArray.length);
-
-    // Close the browser after all tasks are completed
-    await browser.close();
-  });
-
-async function runInParallel(tasks: (() => Promise<any>)[], concurrency: number) {
-  const results: any[] = [];
-  const queue = [...tasks];
-
-  async function runNextTask() {
-    const task = queue.shift();
-    if (task) {
-      const result = await task();
-      if (result != null) {
-        results.push(result);
-      }
-      await runNextTask();
-    }
-  }
-
-  const executing = Array.from({ length: concurrency }, () => runNextTask());
-  await Promise.all(executing);
-  return results;
-}
-
-async function scrapeMangaData(browser: Browser, url: string, title: string, index: number) {
+async function scrapeMangaData(browser: Browser, url: string, title: string, id: number, malId?: number) {
+  malId = malId ?? id;
   try {
     let page = await browser.newPage();
     page.setDefaultNavigationTimeout(0);
@@ -88,29 +16,22 @@ async function scrapeMangaData(browser: Browser, url: string, title: string, ind
     await page.exposeFunction('notEmpty', notEmpty);
     await page.exposeFunction('parseIntImproved', parseIntImproved);
     await page.exposeFunction('parseFloatImproved', parseFloatImproved);
-    page.on('console', (msg) => 
+
+    page.on('console', (msg) =>
       console.log(chalk.gray(`[Browser] ${msg.text()}`))
     );
 
     // rate limit detector
     if (await page.$('.g-recaptcha') != null) {
-      console.error('Rate limit detected, current index:', index);
+      console.error('Rate limit detected, current index:', id);
       return null;
     }
 
-    const genres = await page.$$eval('span[itemprop="genre"]', (elements) => {
+    let genres = await page.$$eval('span[itemprop="genre"]', (elements) => {
       return elements
         .map((element) => element?.textContent?.trim())
         .filter(notEmpty)
-      // .filter();
     });
-
-    // ignore if not wanted genre
-    if (!genres.includes(WANTED_GENRE)) {
-      console.log(chalk.yellow(index + ': ' + title + ' skipped'));
-      await page.close();
-      return null;
-    }
 
     let description = await page.$eval('span[itemprop="description"]',
       (element) => element?.textContent?.trim()
@@ -153,12 +74,14 @@ async function scrapeMangaData(browser: Browser, url: string, title: string, ind
     }
 
     let authors = await page.$$eval('.information.studio.author a', (elements) => elements.map((element) => element?.textContent?.trim()).filter(notEmpty));
+
     let score = await page.$eval('.score-label', (element) => parseFloatImproved(element.textContent));
+
     let members = await page.$eval('.numbers.members', (element) => parseIntImproved(element.textContent));
 
     // download image
     let imageUrl = await page.$eval('.leftside img', (element: { src: any; }) => element.src)
-    let imageLocation = process.env.MANGA_IMAGES_PATH + '/' + index + '.jpg';
+    let imageLocation = process.env.MANGA_IMAGES_PATH + '/' + malId + '.jpg';
     let imageFile = fs.createWriteStream(imageLocation);
 
     if (!fs.existsSync(imageLocation)) {
@@ -211,7 +134,6 @@ async function scrapeMangaData(browser: Browser, url: string, title: string, ind
         // ignore unpopular characters
         // if you can't even surpass 100 popularity
         // I don't think computation time is worth it
-
         if (popularity < 100) {
           return null;
         }
@@ -220,9 +142,8 @@ async function scrapeMangaData(browser: Browser, url: string, title: string, ind
       }).filter((e) => e != null).sort((a, b) => b!.popularity - a!.popularity);
     })
 
-    // const newManga = new Manga(index, title, alternativeTitle, authors, description, background, genres, characters, members, score);
     const newManga = {
-      id: index,
+      id: malId,
       title,
       alternativeTitle,
       authors,
@@ -234,43 +155,20 @@ async function scrapeMangaData(browser: Browser, url: string, title: string, ind
       score,
       url,
     };
-    // console.log(newManga)
-    console.log(chalk.green(index + ': ' + title + ' finished'));
-    // index++;
 
-    // Add delay to prevent being identify as bot
-    await new Promise(resolve => setTimeout(resolve, DELAY));
+    console.log(chalk.green(id +' ('+malId+'): ' + title + ' finished'));
+
     // Close the page after scraping data
     await page.close();
 
     // save current progress
-    // writeJsonFile('./manga_data/'+index+'.json', newManga);
-    writeJsonFile(`${process.env.MANGA_DATA_PATH}/${index}-${title}.json`, newManga);
+    writeJsonFile(`${process.env.MANGA_DATA_PATH}/${malId}-${title}.json`, newManga);
 
     return newManga;
   } catch (error) {
     console.error('Error during scraping for', title, ':', error);
     return null;
   }
-}
-
-function writeJsonFile(filePath: string, data: any) {
-  const jsonString = JSON.stringify(data, null, 2);
-
-  fs.mkdir(process.env.MANGA_DATA_PATH, { recursive: true }, (err: any) => {
-    if (err) {
-      console.error('Error creating JSON folder:', err);
-    }
-  });
-
-  filePath = filePath.replace(/[\?%*:|"<>]/g, '-');
-  fs.writeFile(filePath, jsonString, { flag: "w", encoding: "utf-8" }, (err: any) => {
-    if (err) {
-      console.error('Error writing JSON file:', err);
-    } else {
-      // console.log('JSON file has been written successfully:', filePath);
-    }
-  });
 }
 
 function parseIntImproved(string?: string | null) {
@@ -303,3 +201,28 @@ function parseFloatImproved(string?: string | null) {
 function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
   return value != null && value != undefined;
 }
+
+function writeJsonFile(filePath: string, data: any) {
+  const JsonPath = path.resolve(filePath)
+  const folderPath = path.dirname(JsonPath)
+  const jsonString = JSON.stringify(data, null, 2);
+
+  fs.mkdir(folderPath, { recursive: true }, (err: any) => {
+    if (err) {
+      console.error('Error creating JSON folder:', err);
+    }
+  });
+
+  filePath = filePath.replace(/[\?%*:|"<>]/g, '-');
+  fs.writeFile(filePath, jsonString, { flag: "w", encoding: "utf-8" }, (err: any) => {
+    if (err) {
+      console.error('Error writing JSON file:', err);
+    }
+  });
+}
+
+// Test
+// puppeteer.launch({ headless: false }).then(async (browser) => {
+//   scrapeMangaData(browser, "https://myanimelist.net/manga/1/Monster", "Monster", 1)
+// })
+export default scrapeMangaData;
---
+++ Tools/scraper_prep.ts
@@ -0,0 +1,150 @@
+import puppeteer from "puppeteer";
+import fs from "fs";
+import * as csv from "fast-csv";
+import scrapeMangaData from "./scraper_func.js";
+
+// const url = "https://anilist.co/search/manga?genres=Romance&genres=Fantasy&format=MANGA&country%20of%20origin=JP"
+const apiUrl = "https://graphql.anilist.co"
+const delay = 1000
+const startIndex = 90
+const endIndex = 100
+
+async function main() {
+
+  // Fetch top 50 romance manga from anilist
+  let anilistList = await fetchManga(endIndex, "Romance")
+
+  let mangaList: any[] = []
+  fs.createReadStream("./manga_dataset.csv")
+    .pipe(csv.parse({ headers: true }))
+    .on("data", (data: any) => {
+      data["TitleLower"] = data.Title.toLowerCase().trim().replace(/[^a-zA-Z0-9]/g, "")
+      if (data.Type.trim() == "Manga" && parseInt(data.Members) > 10000)
+        mangaList.push(data)
+    })
+    .on("end", async () => {
+      let toBeScraped: any[] = []
+
+      anilistList.forEach((anime: string) => {
+        let formattedAnime = anime.toLowerCase().trim().replace(/[^a-zA-Z0-9]/g, "")
+
+        let bestMatch = mangaList.reduce((prev, curr) => {
+          let prevDistance = levenshteinDistance(prev.TitleLower, formattedAnime)
+          let currDistance = levenshteinDistance(curr.TitleLower, formattedAnime)
+          return prevDistance < currDistance ? prev : curr
+        })
+
+        toBeScraped.push(bestMatch)
+      })
+
+      // Start scraping
+      let browser = await puppeteer.launch({ headless: true })
+      toBeScraped = toBeScraped.map((entry, index) => {
+        entry["index"] = index
+        return entry
+      })
+      let scrapeTasks = toBeScraped.slice(startIndex,endIndex).map((entry) => {
+        let index = entry["index"]
+        let title = entry.Title
+        let url = entry.page_url
+        let malId = url.match(/\/(\d+)\//)[1]
+        // let index = entry['']
+        return () => scrapeMangaData(browser, url, title, index, malId)
+      })
+
+      // No parallel because of rate limiting
+      for (let i = 0; i < scrapeTasks.length; i++) {
+        await scrapeTasks[i]()
+        await new Promise(resolve => setTimeout(resolve, delay))
+      }
+
+      // Close the browser after all tasks are completed
+      await browser.close()
+    })
+}
+
+function levenshteinDistance(str1: string, str2: string) {
+  const m = str1.length;
+  const n = str2.length;
+
+  // Create a 2D array to store the distances
+  const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+  // Initialize the first row and column
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = i;
+  }
+
+  for (let j = 0; j <= n; j++) {
+    dp[0][j] = j;
+  }
+
+  // Fill in the rest of the array
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      const cost = str1[i - 1] === str2[j - 1] ? 0 : 1;
+      dp[i][j] = Math.min(
+        dp[i - 1][j] + 1,
+        dp[i][j - 1] + 1,
+        dp[i - 1][j - 1] + cost
+      );
+    }
+  }
+
+  // The bottom-right cell contains the Levenshtein distance
+  return dp[m][n];
+}
+
+async function fetchManga(number: number, genre: string) {
+  let reqNum = Math.ceil(number / 50)
+  let mangaList: string[] = []
+
+  for (let i = 0; i < reqNum; i++) {
+    let query = `
+    query($page: Int, $perPage: Int, $genre: String) {
+      Page(page: $page, perPage: $perPage) {
+        pageInfo {
+          total
+        }
+        media(
+          format: MANGA
+          type: MANGA
+          sort: POPULARITY_DESC
+          genre: $genre
+          countryOfOrigin: "JP"
+        ) {
+          id
+          title {
+            romaji
+          }
+        }
+      }
+    }
+    `
+    let variables = {
+      page: i,
+      perPage: 50,
+      genre: genre
+    }
+
+    let response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ query, variables })
+    })
+
+    let json = await response.json()
+    let mangaName: string[] = json.data.Page.media.map((manga: any) => {
+      return manga.title.romaji
+    })
+    mangaList = mangaList.concat(mangaName)
+  }
+
+  return mangaList
+}
+
+main().then(() => {
+  console.log("Done")
+})
